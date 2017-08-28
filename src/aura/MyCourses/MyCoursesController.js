5767a52c969e2e334e9fcf770c81fad03159e940
nsoberon <nico.soberon@gmail.com>
2017-08-28 09:13:24 -0300

    My Courses

--- src/aura/MyCourses/MyCoursesController.js
+++ src/aura/MyCourses/MyCoursesController.js
@@ -1,12 +1,34 @@
 ({
     doInit: function(component, event, helper){                
-        var fieldset = [{apiName: "FieloELR__Course__r.Name", type: "output", label: "Name", showLabel: true}, {apiName: "FieloELR__Progress__c", type: "subcomponent", subcomponent: "ProgressBar", label: "Progress", showLabel: true}]
+        var fieldset = [{apiName: "Name", type: "subcomponent", subcomponent: "ShowCourse", label: "Name", showLabel: true}, {apiName: "FieloELR__Progress__c", type: "subcomponent", subcomponent: "ProgressBar", label: "Progress", showLabel: true}]
+        var modulesFieldset = [{apiName: "Name", type: "output", label: "Name", showLabel: true}, {apiName: "FieloELR__Description__c", type: "output", label: "Description", showLabel: true}, {apiName: "FieloELR__ApprovalGrade__c", type: "output", label: "Approval Grade", showLabel: true}];
         component.set('v.title', 'My Courses');
+        component.set('v.modulesFieldset', modulesFieldset);
         component.set('v.fieldset', fieldset);
     },
-     updateMember: function(component, event, helper){
+    updateMember: function(component, event, helper){
         var member = event.getParam('member');        
         component.set('v.member', member);        
         helper.loadCourses(component, event, helper);        
+    },
+    showCourse: function(component, event, helper){         
+        var course = event.getParam('record');        
+        var modulesList = [];
+        var modules = course.modules;        
+        for(var i = 0; i < modules.length; i++){
+            var newModule = modules[i].module;
+            newModule.moduleResponses = modules[i].moduleResponses;
+            newModule.numberOfAttempts = modules[i].numberOfAttempts;
+            newModule.isApproved = modules[i].isApproved;
+            modulesList.push(newModule);
+        }                
+        component.set('v.modules', modulesList);
+        component.set('v.courseTitle', course.Name);
+        component.set('v.showMyCourses', false);
+        component.set('v.showCourse', true);            
+    },
+    showCoursesList: function(component, event, helper){
+        component.set('v.showMyCourses', true);        
+        component.set('v.showCourse', false); 
     }
 })
--- src/aura/MyCourses/MyCoursesHelper.js
+++ src/aura/MyCourses/MyCoursesHelper.js
@@ -1,21 +1,33 @@
 ({
     loadCourses : function(component, event, helper) {
-        var member = component.get('v.member');                
+        var member = component.get('v.member');
+        var fieldset = ['Name','FieloELR__Description__c'];
         if(member){
             var spinner = $A.get("e.c:ToggleSpinnerEvent");
             if(spinner){
                 spinner.setParam('show', true);
                 spinner.fire();    
             }                
-            var action = component.get('c.getMyCourses');
+            var action = component.get('c.getCourses');
             action.setParams({
-                'member': member
+                'member': member,
+                'fieldset': fieldset
             })
             // Add callback behavior for when response is received
             action.setCallback(this, function(response) {
                 var state = response.getState();
                 if (component.isValid() && state === 'SUCCESS') {                    
-                    var myCourses = JSON.parse(response.getReturnValue());
+                    var myCourses = [];
+                    var courses = JSON.parse(response.getReturnValue());
+                    console.log(courses);
+                    courses.forEach(function(course){
+                        if(course.courseStatus){
+                            var newCourse = course.courseStatus;
+                            newCourse.Name = course.course.Name;
+                            newCourse.modules = course.modules;
+                            myCourses.push(newCourse);
+                        }
+                    })
                     component.set('v.myCourses', myCourses); 
                     component.set('v.showMyCourses', false);
                     component.set('v.showMyCourses', true);
