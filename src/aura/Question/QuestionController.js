e1167622c3e26f4e57db30e2ac0cae7152dc0ad4
nsoberon <nico.soberon@gmail.com>
2017-09-04 16:05:08 -0300

    Quiz

--- src/aura/Question/QuestionController.js
+++ src/aura/Question/QuestionController.js
@@ -1,4 +1,14 @@
 ({
-	myAction : function(component, event, helper) {
-	}
+	doInit : function(component, event, helper) {
+        console.log('nico');
+        var question = component.get('v.question');
+        console.log(question);
+        component.set('v.title', question.FieloELR__QuestionText__c);
+        component.set('v.options', question.FieloELR__AnswerOptions__r.records);        
+        component.set('v.renderQuestion', true);
+        console.log('nico');
+	},
+    setAnswer: function(component, event, helper){
+        
+    }
 })
