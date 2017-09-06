ab32094f3c8eda6bc4e9609e99e4d007ea64337a
nsoberon <nico.soberon@gmail.com>
2017-09-06 15:52:24 -0300

    Question

--- src/aura/Question/QuestionController.js
+++ src/aura/Question/QuestionController.js
@@ -1,14 +1,49 @@
 ({
-	doInit : function(component, event, helper) {
-        console.log('nico');
-        var question = component.get('v.question');
-        console.log(question);
+    doInit : function(component, event, helper) {        
+        var question = component.get('v.question');        
         component.set('v.title', question.FieloELR__QuestionText__c);
         component.set('v.options', question.FieloELR__AnswerOptions__r.records);        
         component.set('v.renderQuestion', true);
-        console.log('nico');
-	},
+        var type = component.get('v.type');
+        if (type == 'Matching Options') {
+            var options = {};
+            var i = 0;
+            question.FieloELR__AnswerOptions__r.records.forEach(function(option){
+               options[i] = option.FieloELR__AnswerOptionText__c; 
+               i++;
+            });
+            component.set('v.matchingOptions', options);
+        }
+    },
     setAnswer: function(component, event, helper){
+        var answerLabel = event.getSource().get('v.label');
+        var answerValue = event.getSource().get('v.value');        
+        var moduleAnswer = window.localStorage.getItem('moduleAnswer');        
+        moduleAnswer = JSON.parse(moduleAnswer);        
+        var moduleId = component.get('v.moduleId');        
+        var question = component.get('v.question').Id;
+        var type = component.get('v.type');           
         
+        if (type == 'Single Choice' || type == 'Statement') {                                    
+            moduleAnswer[moduleId][question] =  answerLabel;
+        } else if (type == 'Multiple Choice') {                        
+            if(answerValue){
+                var answer = moduleAnswer[moduleId][question] || {};            
+                answer[answerLabel] = answerValue;
+                moduleAnswer[moduleId][question] =  answer;                
+            } else {
+                delete moduleAnswer[moduleId][question][answerLabel];
+            }
+            
+        } else if (type == 'Short Answer') {
+            var answer = component.get('v.answer');
+            moduleAnswer[moduleId][question] =  answer;
+        } else if (type == 'Matching Options') {            
+            var matchingOptions = component.get('v.matchingOptions');
+            var newAnswer = moduleAnswer[moduleId][question] || {};
+            newAnswer[matchingOptions[answerValue]] = answerLabel;
+            moduleAnswer[moduleId][question] = newAnswer;
+        }
+        window.localStorage.setItem('moduleAnswer', JSON.stringify(moduleAnswer));        
     }
 })
--- src/aura/Quiz/QuizController.js
+++ src/aura/Quiz/QuizController.js
@@ -1,4 +1,11 @@
 ({
-	myAction : function(component, event, helper) {
-	}
+	doInit : function(component, event, helper) {
+        var moduleId = component.get('v.module').Id;
+        var answer = {};
+        answer[moduleId] = {};
+        window.localStorage.setItem('moduleAnswer', JSON.stringify(answer));
+	},
+    showAnswer : function(component, event, helper) {
+        console.log(JSON.parse(window.localStorage.getItem('moduleAnswer')));
+    }
 })
