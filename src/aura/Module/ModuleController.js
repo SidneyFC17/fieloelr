361a318de8a8749e546eb88a238dfc6fc1136cfd
nsoberon <nico.soberon@gmail.com>
2017-09-14 18:43:38 -0300

    Module Content

--- src/aura/Module/ModuleController.js
+++ src/aura/Module/ModuleController.js
@@ -1,7 +1,8 @@
 ({
     doInit: function(component, event, helper){
-        var module = component.get('v.module');
+        var module = component.get('v.module');                        
   		component.set('v.questions', module.questions);
-        component.set('v.showQuestions', true);			
+        component.set('v.showQuestions', true);
+        helper.loadContent(component, event, helper);        
     }
 })
--- src/aura/Module/ModuleHelper.js
+++ src/aura/Module/ModuleHelper.js
@@ -13,10 +13,12 @@
             // Add callback behavior for when response is received
             getContent.setCallback(this, function(response) {
                 var state = response.getState();                
-                if (component.isValid() && state === 'SUCCESS') {                    
-                    component.set('v.content', response.getReturnValue());
-                    component.set('v.showContent', true);                                        
-                    
+                if (component.isValid() && state === 'SUCCESS') {                                        
+                    var moduleResponse = response.getReturnValue();
+                    component.set('v.content', moduleResponse);                    
+                    if(moduleResponse[contentField]){
+                        component.set('v.showContent', true);                                            
+                    }                                        
                 }else {
                     console.log('Failed with state: ' + state);
                 }
